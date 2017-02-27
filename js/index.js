62d0f80a1f58b320b8bc4945a61f9d3e1140fb1e
srikanth r <srikanthr.0402@gmail.com>
2017-02-28 00:42:56 +0530

    Added header for the webpage. Added initial template to work on.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,95 @@
 $(document).ready(function() {
-	console.log("ready");
-	$("#loader_div").css("display", "none");
-	$("#dummy").css("display", "block");
+	$("#loader_div").hide();
+	$("#body").show();
+	var winHeight = $(window).height();
+	$("#header").css("height", winHeight );
+	$("#personal_info_div").css("height", winHeight );
+	$("#experience_div").css("height", winHeight );
+	$("#education_div").css("height", winHeight );
+	$("#skills_div").css("height", winHeight );
+	$("#contact_div").css("height", winHeight );
+	header();
+
+	$("#personal_info_trigger, #experience_trigger, #education_trigger, #skills_trigger, #contact_trigger").click(function() {
+		$(this).parent().children().removeClass("active");
+		$(this).addClass("active");
+		scroll($(this).attr("data-div"));
+	});
+
+	setWayPoint("#personal_info_div");
+	setWayPoint("#experience_div");
+	setWayPoint("#education_div");
+	setWayPoint("#skills_div");
+	setWayPoint("#contact_div");
+	$("#header").waypoint(function() {
+		var id =$(this.element).attr('id');
+		console.log(id);
+		removedviewattr = "opaque";
+		viewattr = "transparent";
+		$("header").removeClass("opaqueheader").addClass("transparentheader");
+		$(".table_of_contents").children().removeClass("active");
+	}, {offset:'-75%'});
 });
+
+var viewattr = "transparent";
+var removedviewattr = "opaque";
+
+function setWayPoint(section) {
+	var waypointFunc = function() {
+		var id =$(this.element).attr('id');
+		console.log(id);
+		removedviewattr = "transparent";
+		viewattr = "opaque";
+		$("header").removeClass("transparentheader").addClass("opaqueheader");
+		var $trigger = $($(this.element).attr("data-trigger"));
+		$trigger.parent().children().removeClass("active");
+		$trigger.addClass("active");
+	};
+	$(section).waypoint(waypointFunc, {offset:'15%'});
+	$(section).waypoint(waypointFunc, {offset:'-15%'});
+}
+
+function scroll(section) {
+	$('html, body').animate({
+		scrollTop: $(section).offset().top
+	}, 500);
+}
+
+function header() {
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('header').outerHeight();
+
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll) {
+			hasScrolled();
+			didScroll = false;
+		}
+	}, 250);
+
+	function hasScrolled() {
+		var st = $(this).scrollTop();
+
+		if(Math.abs(lastScrollTop - st) <= delta)
+			return;
+
+		if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('header').removeClass('nav-down').removeClass(removedviewattr+'nav-up').addClass(viewattr+'nav-up');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	        	$('header').removeClass(viewattr+'nav-up').removeClass(removedviewattr+'nav-up').addClass('nav-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
+}
+
+
